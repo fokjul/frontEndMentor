3e3ce28dedac3b7c38ae19cb69cbc9be4cfb2683
fokjul <40266249+fokjul@users.noreply.github.com>
2023-08-31 15:56:08 -0700

    Add files via upload

---
+++ advice-generator-app-main/scripts/script.js
@@ -0,0 +1,20 @@
+const adviceId =  document.createElement('p');
+adviceId.classList.add('adviceId');
+document.querySelector('.advice-container').prepend(adviceId);
+const adviceText =  document.createElement('p');
+adviceText.classList.add('adviceText');
+adviceId.insertAdjacentElement('afterend', adviceText);
+const advicePlaceholder = document.querySelector('.advicePlaceholder');
+
+document.querySelector('button').addEventListener('click', function() {
+    advicePlaceholder.remove();
+    fetch('https://api.adviceslip.com/advice')
+    .then (response => response.json())
+    .then (json => {
+        document.querySelector('.adviceId').innerHTML = `Advice #${json.slip.id}`;
+        document.querySelector('.adviceText').innerHTML = `"${json.slip.advice}"`;
+    })
+    .catch ( error =>{
+        console.log('error:' + error.message)
+    });
+})
